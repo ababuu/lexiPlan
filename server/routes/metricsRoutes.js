c8bd6f09a4e5fa48f8fdce74bb92b1a72fdd4308
ababu <ababualemu123@gmail.com>
2025-12-29 21:45:36 +0300

    refactor: remove metrics files

--- server/routes/metricsRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getMetrics } from "../controllers/metricsController.js";
-
-const router = express.Router();
-
-// Lightweight metrics endpoint (not mounted by default)
-router.get("/", getMetrics);
-
-export default router;
