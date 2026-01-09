a412d0158854e01e6b996bc5bc01963f8d29af0b
ababu <ababualemu123@gmail.com>
2026-01-09 19:47:11 +0300

    chore: adjust sidebar stacking order

--- client/src/components/layout/Sidebar.jsx
+++ client/src/components/layout/Sidebar.jsx
@@ -47,7 +47,7 @@ const Sidebar = ({ isOpen, closeSidebar }) => {
     <>
       {/* Mobile sidebar - slides in from left */}
       <div
-        className={`fixed lg:relative lg:translate-x-0 inset-y-0 left-0 z-50 w-64 sm:w-72 lg:w-60 bg-[hsl(var(--nav-background))] border-r-[0.5px] border-border h-full flex flex-col backdrop-blur-sm overflow-hidden transition-transform duration-300 ease-in-out ${
+        className={`fixed lg:relative lg:translate-x-0 inset-y-0 left-0 z-40 w-64 sm:w-72 lg:w-60 bg-[hsl(var(--nav-background))] border-r-[0.5px] border-border h-full flex flex-col backdrop-blur-sm overflow-hidden transition-transform duration-300 ease-in-out ${
           isOpen ? "translate-x-0" : "-translate-x-full"
         }`}
       >
